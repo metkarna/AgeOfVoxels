d158bdd0843063fa19bf7ae641ab332bf5783d39
metkarna <metkarna@gmail.com>
2019-04-10 03:52:06 +0500

    Сделал меню, затычку меню опций, выход из игры. Переписал функции в ButtonHandler - теперь там порядок.

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -9,25 +9,22 @@ using System;
 public class ButtonHandler : MonoBehaviour, IPointerClickHandler
 {
     #region Fields
-    // имя нажатой кнопки
+    // Имя нажатой кнопки
     private string _eventData;
-
-    // хранится ссылка на метод который должен будет выполнен при нажатии
+    // Хранится ссылка на метод который должен будет выполнен при нажатии
     private delegate void callMe();
     private callMe callAction;
     #endregion
 
     #region PlantClickCalledMethods
-    // запланировать выполнение метода buildTower при нажатии на кнопку
+    // Запланировать выполнение метода buildTower при нажатии на кнопку
     public void BuildTowerClick() => callAction = BuildRedTower;
-
     // Вызвать постройку башни и выключенить меню
     public void BuildRedTower()
     {
         var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
         ui.isFrozen = false;
         
-
         var goldController = GameObject.FindObjectOfType(typeof(GoldConroller)) as GoldConroller;
         if (goldController.BuyTower(_eventData))
         {
@@ -35,129 +32,119 @@ public class ButtonHandler : MonoBehaviour, IPointerClickHandler
             var towerSpawner = GameObject.FindObjectOfType(typeof(TowerSpawner)) as TowerSpawner;
             towerSpawner.SpawnTowerRed(_eventData, plant);
         }
-
         ui.buildMenu.SetActive(false);
         Debug.Log("tower tryed build ");
     }
     #endregion
 
-    #region OpenBuyUnitsRedClickCalledMethods
     // Покупка и спавн героев игроком
-    // запланировать выполнение метода CreateUnit, OpenBuyUnitsMenuClick, CloseBuyUnitsGame при нажатии на кнопку
+    #region OpenBuyUnitsRedClickCalledMethods
+    // Запланировать выполнение метода CreateUnit при нажатии на кнопку
     public void BuyUnitClickRed() => callAction = CreateUnitRed;
-    public void OpenBuyUnitsMenuClickRed() => callAction = OpenBuyUnitsGameRed;
-    public void CloseBuyUnitsMenuClickRed() => callAction = CloseBuyUnitsGameRed;
-    // Открыть меню покупки
-    private void OpenBuyUnitsGameRed()
-    {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = true;
-        ui.buyUnitsMenuRed.SetActive(true);
-    }
-    // Закрыть меню покупки
-    private void CloseBuyUnitsGameRed()
-    {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = false;
-        ui.buyUnitsMenuRed.SetActive(false);
-    }
-
+    public void BuyUnitClickBlue() => callAction = CreateUnitBlue;
     // Вызвать создание юнита
     private void CreateUnitRed()
     {
         var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = false;
-         var goldController = GameObject.FindObjectOfType(typeof(GoldConroller)) as GoldConroller;
-         if (goldController.BuyUnit(_eventData))
-         {
-            var unitSpawner = GameObject.FindObjectOfType(typeof(UnitSpawner)) as UnitSpawner;
+        var unitSpawner = GameObject.FindObjectOfType(typeof(UnitSpawner)) as UnitSpawner;
+        var goldController = GameObject.FindObjectOfType(typeof(GoldConroller)) as GoldConroller;
+        if (goldController.BuyUnit(_eventData))
+        {
             unitSpawner.SpawnUnitsRed(_eventData);
+        }
+    }
+    private void CreateUnitBlue()
+    {
+        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
+        var unitSpawner = GameObject.FindObjectOfType(typeof(UnitSpawner)) as UnitSpawner;
+		unitSpawner.SpawnUnitsBlue(_eventData);
+    }
+    #endregion
+
+    #region MenusCalledMethods
+    // Запланировать выполнение метода OpenSceneClick при нажатии на кнопку
+    public void OpenMenuClick() => callAction = OpenMenu;
+    public void CloseMenuClick() => callAction = CloseMenu;
+    // Открыть сцену
+    private void OpenMenu()
+    {
+        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
+        // Название нажатой кнопки вызывает открытие соответствующего меню
+        switch (_eventData)
+        {   
+            case "btnOpenPause":
+                Time.timeScale = 0;
+                ui.isFrozen = true;
+                ui.pauseGameMenu.SetActive(true);
+                break;
+            case "btnOpenBuyUnitsBlue":
+                ui.buyUnitsMenuBlue.SetActive(true);
+                break;
+            case "btnOpenBuyUnitsRed":
+                ui.buyUnitsMenuRed.SetActive(true);
+                break;
+        }
+    }
+    private void CloseMenu()
+    {
+        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
+        // Название нажатой кнопки вызывает зыкрытие соответствующего меню
+        switch (_eventData)
+        {
+            case "btnClosePause":
+                Time.timeScale = 1;
+                ui.isFrozen = false;
+                ui.pauseGameMenu.SetActive(false);
+                break;
+            case "btnCloseBuyUnitsBlue":
+                ui.buyUnitsMenuBlue.SetActive(false);
+                break;
+            case "btnCloseBuyUnitsRed":
+                ui.buyUnitsMenuRed.SetActive(false);
+                break;
+            case "btnCloseBuyTowersRed":
+                ui.isFrozen = false;
+                ui.buildMenu.SetActive(false);
+                break;
         }
     }
     #endregion
 
-    #region OpenBuyUnitsBlueClickCalledMethods
-    // Покупка и спавн героев противником
-    // запланировать выполнение метода CreateUnit, OpenBuyUnitsMenuClick, CloseBuyUnitsGame при нажатии на кнопку
-    public void BuyUnitClickBlue() => callAction = CreateUnitBlue;
-    public void OpenBuyUnitsMenuClickBlue() => callAction = OpenBuyUnitsGameBlue;
-    public void CloseBuyUnitsMenuClickBlue() => callAction = CloseBuyUnitsGameBlue;
-    // Открыть меню покупки
-    private void OpenBuyUnitsGameBlue()
+    #region OpenScenesCalledMethods
+    // Запланировать выполнение метода OpenSceneClick при нажатии на кнопку
+    public void OpenSceneClick() => callAction = OpenScene;
+    // Открыть сцену
+    private void OpenScene()
     {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = true;
-        ui.buyUnitsMenuBlue.SetActive(true);
-    }
-    // Закрыть меню покупки
-    private void CloseBuyUnitsGameBlue()
-    {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = false;
-        ui.buyUnitsMenuBlue.SetActive(false);
-    }
-
-    // Вызвать создание юнита (без учета денег)
-    private void CreateUnitBlue()
-    {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        //ui.isFrozen = false;
-        // var goldController = GameObject.FindObjectOfType(typeof(GoldConroller)) as GoldConroller;
-        // if (goldController.BuyUnit(_eventData))
-        // {
-            var unitSpawner = GameObject.FindObjectOfType(typeof(UnitSpawner)) as UnitSpawner;
-            unitSpawner.SpawnUnitsBlue(_eventData);
-        // }
-    }
-    #endregion
-
-    #region PauseGameClickCalledMethods
-    // запланировать выполнение метода OpenPauseGame при нажатии на кнопку
-    public void OpenPauseMenuClick() => callAction = OpenPauseGame;
-    // запланировать выполнение метода ClosePauseGame при нажатии на кнопку
-    public void ClosePauseMenuClick() => callAction = ClosePauseGame;
-    // Открыть меню паузы
-    private void OpenPauseGame()
-    {
-        Time.timeScale = 0;
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        ui.isFrozen = true;
-        ui.pauseGameMenu.SetActive(true);
-    }
-    // Закрыть меню паузы
-    private void ClosePauseGame()
-    {
-        Time.timeScale = 1;
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        ui.isFrozen = false;
-        ui.pauseGameMenu.SetActive(false);
-    }
-    #endregion
-
-
-    #region PauseGameClickCalledMethods
-    // запланировать выполнение метода ClosePauseGame при нажатии на кнопку
-    public void CloseBuyTowersMenuClick() => callAction = CloseBuyTowersMenuGame;
-    // Закрыть меню покупки башен
-    private void CloseBuyTowersMenuGame()
-    {
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        ui.isFrozen = false;
-        ui.buildMenu.SetActive(false);
-    }
-    #endregion
-
-    #region MenuClickCalledMethods
-    // запланировать выполнение метода OpenMenu при нажатии на кнопку
-    public void OpenMenuClick() => callAction = OpenMenu;
-    // Открыть меню уровней( или чего там у нас??????)
-    private void OpenMenu()
-    {
-        SceneManager.LoadScene("Menu");
-        Time.timeScale = 0;
-        var ui = GameObject.FindObjectOfType(typeof(UIController)) as UIController;
-        ui.isFrozen = true;
-        ui.pauseGameMenu.SetActive(true);
+        // Название нажатой кнопки вызывает загрузку соответствующиего сцены
+        switch (_eventData)
+        {
+            case "level-test":
+                Debug.Log("Открытие сцены: Тестовый уровень");
+                SceneManager.LoadScene("test-map");
+                break;
+            case "level 1":
+                Debug.Log("Открытие сцены: Уровень Map1-1");
+                SceneManager.LoadScene("Map1-1");
+                break;
+            case "btnGoToMenu":
+                Debug.Log("Открытие сцены: Меню");
+                SceneManager.LoadScene("Menu");
+                break;
+            case "btnGoToLevels":
+                Debug.Log("Открытие сцены: Уровни");
+                SceneManager.LoadScene("Levels");
+                break;
+            case "btnGoToOptions":
+                Debug.Log("Открытие сцены: Уровни");
+                SceneManager.LoadScene("Options");
+                break;
+            case "btnExit":
+                Debug.Log("Закрытие игры");
+                UnityEditor.EditorApplication.isPlaying = false;
+                //Application.Quit();
+                break;                
+        }
     }
     #endregion
 
@@ -169,5 +156,4 @@ public class ButtonHandler : MonoBehaviour, IPointerClickHandler
         callAction();
         Debug.Log(_eventData);
     }
-
 }
